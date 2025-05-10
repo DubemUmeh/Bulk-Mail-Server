51cd05b3f2e7008d277a02292a726f20985c7413
Dubem-Umeh-Raphael <raphaelumeh21@gmail.com>
2025-05-10 22:00:04 +0000

    modified allowed origin

--- newServer.js
+++ newServer.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
 'https://bulk-mail-sender-adms.onrender.com',
-'https://localhost:5173',
+'http://localhost:5173',
 ];
 
 app.use(cors({
